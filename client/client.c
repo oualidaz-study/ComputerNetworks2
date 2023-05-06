7c9477070bb4657f25f26ec952f65615430074c0
Oualid Azzeggarh <oualid@elnino.tech>
2023-05-06 12:54:24 +0200

    UDP Socket Connection Working. Music is played at client-side

--- client/client.c
+++ client/client.c
@@ -4,14 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
+#include "player.h"
+#include "wave.h"
 #include "player.h"
 
 #define BIND_PORT 1235
+#define BUFFER_SIZE 2000
 
-int main(int argc, char** argv) {
+
+int wave_play(int argc, char** argv){
     int buffer_size = 1024*1024;  // TODO: Get buffersize using commandline args.
     int bind_port = BIND_PORT;
 
@@ -54,6 +64,227 @@ int main(int argc, char** argv) {
     // clean up
     free(recvbuffer);
     player_close(&player);
+}
 
-    return 0;
+void clean_buffer(char* message){
+    memset(message, '\0', sizeof(message));
+}
+
+int setup_udp_socket(){
+    int socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if(socket_desc < 0){
+        printf("Error while creating socket\n");
+        return -1;
+    }
+    printf("Socket created successfully\n");
+    return socket_desc;
+}
+
+struct sockaddr_in set_addr(){
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    return addr;
+}
+
+void bind_addr_to_port(int socket_desc, struct sockaddr_in addr){
+    printf("%d", socket_desc);
+
+    if(bind(socket_desc, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        perror("Error: ");
+        exit(EXIT_FAILURE);
+    }
+    printf("Done with binding\n");
+}
+
+void receive_message(int socket_desc, struct sockaddr_in sender_addr, char* sender_msg){
+    int sender_struct_length = sizeof(sender_addr);
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    if (recvfrom(socket_desc, buffer, BUFFER_SIZE, 0,
+         (struct sockaddr*)&sender_addr, &sender_struct_length) < 0){
+        printf("Couldn't receive\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Received message from IP: %s and port: %i\n",
+           inet_ntoa(sender_addr.sin_addr), ntohs(sender_addr.sin_port));
+    strncpy(sender_msg, buffer, BUFFER_SIZE);
+}
+
+void send_message(int socket_desc, struct sockaddr_in receiver_addr, char* msg){
+    int receiver_struct_length = sizeof(receiver_addr);
+    if (sendto(socket_desc, msg, strlen(msg), 0,
+         (struct sockaddr*)&receiver_addr, receiver_struct_length) < 0){
+        perror("Error: ");
+        return -1;
+    }
+}
+
+void connect_to_server(int socket_desc, struct sockaddr_in server_addr){
+    if(connect(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error: ");
+        exit(0);
+    }
+}
+
+void close_socket(int socket_desc){
+    close(socket_desc);
+}
+
+/* Signal handler for stopping playback */
+sig_atomic_t stop = 0;
+static void sigint_handler(int sig) {
+    (void) sig;
+    stop = 1;
+}
+
+char* join_server(int socket_desc){
+    
+
+    //connect
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(1235);
+    server_addr.sin_family = AF_INET;
+
+    connect_to_server(socket_desc, server_addr);
+
+    // Get input from the user:
+    char client_message[2000] = "Hi Server\0";
+    // Send the message to server:
+    send_message(socket_desc, server_addr, client_message);
+
+
+    // Receive the server's response:
+    char* file_name = (char*)malloc(64000 * sizeof(char));
+    if (file_name == NULL) {
+        printf("Error: Failed to allocate memory.\n");
+        exit(EXIT_FAILURE);
+    }
+    memset(file_name, '\0', 64000);
+
+    receive_message(socket_desc, server_addr, file_name);
+    
+    printf("Server's response: %s\n", file_name);
+    return file_name;
+}
+
+int main(int argc, char** argv) {
+    
+    //setup UDP
+    int socket_desc = setup_udp_socket(); // Create UDP socket:
+    char* file_name = join_server(socket_desc);
+
+    //______________________________________________________________________________
+
+    struct wave_file wave;
+    if (wave_open(&wave, file_name) != 0) {
+        return EXIT_FAILURE;
+    }
+
+    const double duration = wave_duration(&wave);
+    const double minutes = floor(duration / 60.);
+    const double seconds = round(fmod(duration, 60.));
+    const int minutes_digits = (minutes == 0 ? 0 : floor(log10(minutes))) + 1;
+
+    /* Print some info */
+    {
+        printf("File:        %s\n",     file_name);
+        printf("Channels:    %hu\n",    wave.fmt->channels);
+        printf("Sample rate: %u\n",     wave.fmt->sample_rate);
+        printf("Duration:    %.1f s\n", duration);
+    }
+
+    struct player player;
+    if (player_open(&player, wave.fmt->channels, wave.fmt->sample_rate) != 0) {
+        wave_close(&wave);
+        return EXIT_FAILURE;
+    }
+
+    /* Install signal handler to cancel playback */
+    struct sigaction sighandler = {
+        .sa_handler = sigint_handler,
+        .sa_flags = 0
+    };
+    sigemptyset(&sighandler.sa_mask);
+    sigaction(SIGINT, &sighandler, NULL);
+
+    /* Current position we're reading audio data from */
+    uint8_t* cur = wave.data->data;
+
+    /* Total number of bytes played */
+    size_t played = 0;
+    while (played < wave.data->riff.chunk_size && stop == 0) {
+        /* 1 frame is 1 sample in each channel, so for stereo 16-bit this is 4 bytes */
+        const size_t frames_to_play = 4096;
+        size_t bytes_to_play = frames_to_play * wave.fmt->channels * sizeof(int16_t);
+
+        /* Clamp to max size */
+        if ((played + bytes_to_play) > wave.data->riff.chunk_size) {
+            bytes_to_play = wave.data->riff.chunk_size - played;
+        }
+
+        /* Queue the frames */
+        player_queue(&player, cur, bytes_to_play);
+
+        /* Bookkeeping */
+        played += bytes_to_play;
+        cur += bytes_to_play;
+
+        /* Print progress */
+        {
+            double percent = ((double)played) / wave.data->riff.chunk_size;
+            double passed = duration * percent;
+            double m_passed = floor(passed / 60.);
+            double s_passed = round(fmod(passed, 60.));
+
+            int printed = printf("\r| %*.f:%02.f / %.f:%.f | %3.f%% ", minutes_digits, m_passed, s_passed,
+                                 minutes, seconds, percent * 100.);
+            /* Ignore the carriage return */
+            printed -= 1;
+
+            /* Get terminal size */
+            struct winsize w;
+            ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+            /* 2 characters on both sides for borders */
+            int bar_width = (int)w.ws_col - 4 - printed;
+            int cols_filled = round(bar_width * percent);
+
+            /* Print the progress bar */
+            printf("| ");
+            for (int i = 0; i < cols_filled; ++i) {
+                printf("#");
+            }
+
+            for (int i = 0; i < (bar_width - cols_filled); ++i) {
+                printf(" ");
+            }
+
+            printf(" |");
+
+            /* Ensure our progress bar gets updated nice and fast */
+            fflush(stdout);
+        }
+
+        /* 
+         * Wait until there's 1/8th second of audio remaining in the buffer,
+         * this is effectively our ctrl+c latency
+         */
+        player_wait_for_queue_remaining(&player, 0.125);
+    }
+
+    printf("\n");
+
+    player_close(&player);
+    wave_close(&wave);
+
+    // Close the socket:
+    close(socket_desc);
+
+    return EXIT_SUCCESS;
 }
--- server/server.c
+++ server/server.c
@@ -1,13 +1,17 @@
 /* Skeleton code for Computer Networks 2022 @ LIACS */
 #include <stdlib.h>
-
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include "asp.h"
 #include "wave.h"
 
+#define BUFFER_SIZE 2000
+
 static int asp_socket_fd = -1;
 
-int main(int argc, char** argv) {
-    // TODO: Parse command-line options 
+
+void wave_play(int argc, char** argv){
+        // TODO: Parse command-line options 
     (void) argc;
     const char* filename = argv[1];
 
@@ -22,6 +26,103 @@ int main(int argc, char** argv) {
 
     // Clean up
     wave_close(&wave);
+}
+
+
+void clean_buffer(char* message){
+    memset(message, '\0', sizeof(message));
+}
+
+int setup_udp_socket(){
+    int socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if(socket_desc < 0){
+        printf("Error while creating socket\n");
+        return -1;
+    }
+    return socket_desc;
+}
+
+
+struct sockaddr_in set_addr(){
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1235);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_family = AF_INET;
+    return addr;
+}
+
+void bind_addr_to_port(int socket_desc, struct sockaddr_in addr){
+    printf("%d", socket_desc);
+
+    if(bind(socket_desc, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        perror("Error: ");
+        exit(EXIT_FAILURE);
+    }
+}
+
+struct sockaddr_in receive_message(int socket_desc, struct sockaddr_in sender_addr, char* sender_msg){
+    int sender_struct_length = sizeof(sender_addr);
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    if (recvfrom(socket_desc, buffer, BUFFER_SIZE, 0,
+         (struct sockaddr*)&sender_addr, &sender_struct_length) < 0){
+        printf("Couldn't receive\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Received message from IP: %s and port: %i\n",
+           inet_ntoa(sender_addr.sin_addr), ntohs(sender_addr.sin_port));
+    strncpy(sender_msg, buffer, BUFFER_SIZE);
+    return sender_addr;
+}
+
+void send_message(int socket_desc, struct sockaddr_in receiver_addr, char* msg){
+    int receiver_struct_length = sizeof(receiver_addr);
+    if (sendto(socket_desc, msg, strlen(msg), 0,
+         (struct sockaddr*)&receiver_addr, receiver_struct_length) < 0){
+        perror("Error: ");
+        return -1;
+    }
+}
+
+void close_socket(int socket_desc){
+    close(socket_desc);
+}
+
+
+int main(int argc, char** argv) {
+    //Setup UDP
+    int socket_desc = setup_udp_socket(socket_desc); // Create UDP socket:
+    struct sockaddr_in server_addr = set_addr(server_addr); // Set port and IP:
+    bind_addr_to_port(socket_desc, server_addr); // Bind to the set port and IP:
+    
+    //listen
+
+
+    //printf("Listening for incoming messages...\n\n\0");
+
+
+    do{
+        printf("Listening...\n");
+        // Receive client's message:
+    
+        char client_message[2000];
+        clean_buffer(client_message);
+        struct sockaddr_in client_addr;
+        client_addr = receive_message(socket_desc, client_addr, client_message);
+        printf("Msg from client: %s\n", client_message);
+
+        
+        // Respond to client:
+        const char* filename = argv[1];
+        send_message(socket_desc, client_addr, filename);
+    }while(1);
+    
+    
+    
+    // Close the socket:
+    close_socket(socket_desc);
 
     return EXIT_SUCCESS;
 }
